7b7e4e681203ed05d717204109629f7434f9dc5f
Akasha Urooj <kashay029@gmail.com>
2026-09-13 20:18:17 +0500

    Add Experience component to Experience page

---
+++ src/pages/Experience.jsx
@@ -0,0 +1,11 @@
+import ExperienceComponent from "../components/Experience";
+
+function Experience() {
+  return (
+    <main>
+      <ExperienceComponent />
+    </main>
+  );
+}
+
+export default Experience;
